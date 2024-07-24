ff4f8a9532c4aa63368f4e4ac93bcb3bf9739a22
Caio Felipe <93288204+caioskywalker@users.noreply.github.com>
2024-07-24 17:03:33 -0300

    Delete estrangulamento.novo-servico/estrangulamento.novo-servico directory

--- estrangulamento.novo-servico/estrangulamento.novo-servico/src/main/java/br/com/cfarias/estrangulamento/novo_servico/repository/RepositoryUsuario.java
+++
@@ -1,9 +0,0 @@
-package br.com.cfarias.estrangulamento.novo_servico.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.cfarias.estrangulamento.novo_servico.entity.Usuario;
-
-public interface RepositoryUsuario extends CrudRepository<Usuario, String> {
-
-}
