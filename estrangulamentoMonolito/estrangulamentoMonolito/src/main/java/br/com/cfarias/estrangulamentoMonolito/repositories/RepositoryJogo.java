4f75f62a9dd7bb7143c625467610cb0d7dd0e837
Caio Felipe <93288204+caioskywalker@users.noreply.github.com>
2024-07-24 17:03:48 -0300

    Delete estrangulamentoMonolito/estrangulamentoMonolito directory

--- estrangulamentoMonolito/estrangulamentoMonolito/src/main/java/br/com/cfarias/estrangulamentoMonolito/repositories/RepositoryJogo.java
+++
@@ -1,9 +0,0 @@
-package br.com.cfarias.estrangulamentoMonolito.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.cfarias.estrangulamentoMonolito.entities.Jogo;
-
-public interface RepositoryJogo extends CrudRepository<Jogo, Long> {
-
-}
--- estrangulamentoMonolito/estrangulamentoMonolito/src/main/java/br/com/cfarias/estrangulamentoMonolito/repositories/RepositoryUsuario.java
+++
@@ -1,9 +0,0 @@
-package br.com.cfarias.estrangulamentoMonolito.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.cfarias.estrangulamentoMonolito.entities.Usuario;
-
-public interface RepositoryUsuario extends CrudRepository<Usuario, Long> {
-
-}
